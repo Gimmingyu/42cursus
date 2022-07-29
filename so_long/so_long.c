bb5fef364490de970f24f4f28dd55756570c8fc1
Mingyu Kim <soil0201@gmail.com>
2022-07-29 15:51:15 +0900

    Get .xpm and .png files for Sonic

--- so_long/so_long.c
+++ so_long/so_long.c
@@ -6,18 +6,47 @@
 /*   By: mingkim <mingkim@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/28 16:17:39 by mingkim           #+#    #+#             */
-/*   Updated: 2022/07/28 16:48:39 by mingkim          ###   ########.fr       */
+/*   Updated: 2022/07/29 15:47:34 by mingkim          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
 
-int	main(void)
+void	init(t_ptrs **set_ptr)
 {
-	void	*mlx_ptr;
-	void	*win_ptr;
+	(*set_ptr) = malloc(sizeof(t_ptrs));
+	if (!(*set_ptr))
+		exit_on_error();
+	(*set_ptr)->mlx_ptr = mlx_init();
+	if (!(*set_ptr)->mlx_ptr)
+		exit_on_error();
+	(*set_ptr)->win_ptr = mlx_new_window((*set_ptr)->mlx_ptr, \
+							500, 500, "mingkim");
+	if (!(*set_ptr)->win_ptr)
+		exit_on_error();
+}
 
-	mlx_ptr = mlx_init();
-	win_ptr = mlx_new_window(mlx_ptr, 500, 500, "mlx 42");
-	mlx_loop(mlx_ptr);
+int	destroy(int keydown, t_ptrs *pSet)
+{
+	if (!pSet || !pSet->mlx_ptr || !(pSet->win_ptr))
+		exit_on_error();
+	if (keydown == ESC_KEY)
+		mlx_destroy_window(pSet->mlx_ptr, pSet->win_ptr);
+	return (0);
+}
+
+int	main(int ac, char **av)
+{
+	t_ptrs	*p_set;
+	t_info	info;
+
+	init(&p_set);
+	mlx_hook(p_set->win_ptr, X_EVENT_KEY_PRESS, 0, &destroy, p_set);
+	info.width = 300;
+	info.height = 300;
+	info.img = mlx_xpm_file_to_image(p_set->mlx_ptr, SONIC_RIGHT1, \
+								&info.width, &info.height);
+	mlx_put_image_to_window(p_set->mlx_ptr, p_set->win_ptr, \
+								info.img, info.width, info.height);
+	mlx_loop(p_set->mlx_ptr);
 }
--- so_long/so_long.h
+++ so_long/so_long.h
@@ -6,7 +6,7 @@
 /*   By: mingkim <mingkim@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/28 16:24:13 by mingkim           #+#    #+#             */
-/*   Updated: 2022/07/28 16:48:47 by mingkim          ###   ########.fr       */
+/*   Updated: 2022/07/29 15:47:52 by mingkim          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,5 +18,52 @@
 # include "fcntl.h"
 # include "stdlib.h"
 # include "unistd.h"
+
+# define X_EVENT_KEY_PRESS 2
+# define X_EVENT_KEY_RELEASE 3
+# define X_EVENT_KEY_EXIT 17
+
+# define ESC_KEY 53
+# define W_KEY 13
+# define A_KEY 0
+# define S_KEY 1
+# define D_KEY 2
+
+# define SONIC_RIGHT1 "img/sonic_run_ahead_right1.xpm"
+# define SONIC_RIGHT2 "img/sonic_run_ahead_right2.xpm"
+# define SONIC_RIGHT3 "img/sonic_run_ahead_right3.xpm"
+# define SONIC_RIGHT4 "img/sonic_run_ahead_right4.xpm"
+# define SONIC_LEFT1 "img/sonic_run_ahead_left1.xpm"
+# define SONIC_LEFT2 "img/sonic_run_ahead_left2.xpm"
+# define SONIC_LEFT3 "img/sonic_run_ahead_left3.xpm"
+# define SONIC_LEFT4 "img/sonic_run_ahead_left4.xpm"
+
+typedef struct s_ptr_var_set
+{
+	void	*mlx_ptr;
+	void	*win_ptr;
+}	t_ptrs;
+
+typedef struct s_map
+{
+	int	exit_count;
+	int	entry_count;
+	int	collectible_count;
+	int	width;
+	int	height;
+}	t_map;
+
+typedef struct s_information
+{
+	void	*img;
+	char	*addr;
+	int		bits_per_pixel;
+	int		size_of_line;
+	int		width;
+	int		height;
+	int		endian;
+}	t_info;
+
+int	exit_on_error(void);
 
 #endif
